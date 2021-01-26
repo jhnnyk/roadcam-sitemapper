9b09151b98c8fa7917a9d8a28e7074a5f16a24d2
John Knotts <jhnnyk@gmail.com>
2021-01-26 12:55:52 -0700

    failed attempt at working with nevada api

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import AlaskaRoads from '../pages/alaska/AlaskaRoads.vue';
 import CaliforniaRoads from '../pages/california/CaliforniaRoads.vue';
 import ColoradoRoads from '../pages/colorado/ColoradoRoads.vue';
 import IowaRoads from '../pages/iowa/IowaRoads.vue';
+import NevadaImport from '../pages/nevada/import.vue';
 import OregonRoads from '../pages/oregon/OregonRoads.vue';
 import WashingtonRoads from '../pages/washington/WashingtonRoads.vue';
 
@@ -30,6 +31,11 @@ const routes = [
     path: '/iowa',
     name: 'IowaRoads',
     component: IowaRoads,
+  },
+  {
+    path: '/nevada-import',
+    name: 'NevadaImport',
+    component: NevadaImport,
   },
   {
     path: '/oregon',
